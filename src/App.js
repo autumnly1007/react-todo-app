c3ec43cfd35e0f19f32b31f8a3304d76779979b8
autumnly1007 <autumnly1007@gmail.com>
2022-12-16 22:03:22 +0900

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import Lists from './components/Lists';
 
-function App() {
+export default function App() {
+  const [todoData, setTodoData] = useState([
+    {
+      id: '1',
+      title: '공부하기',
+      completed: true,
+    },
+    {
+      id: '2',
+      title: '청소하기',
+      completed: false,
+    },
+  ]);
+  const [value, setValue] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='todoBlock'>
+        <div className='title'>
+          <h1>할 일 목록</h1>
+        </div>
+        <Lists todoData={todoData} setTodoDate={setTodoData} />
+        <Form value={value} setValue={setValue} setTodoData={setTodoData} />
+      </div>
     </div>
   );
 }
-
-export default App;
